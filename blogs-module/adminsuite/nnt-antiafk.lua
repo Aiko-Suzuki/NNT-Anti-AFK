f7963e3320f66623b775966017844be39d1093f3
AikoSuzuki <maxime-van@outlook.fr>
2019-02-01 03:28:26 -0500

    some change

--- blogs-module/adminsuite/nnt-antiafk.lua
+++ blogs-module/adminsuite/nnt-antiafk.lua
@@ -3,19 +3,19 @@ if (not AikoAntiAFK) then return end
 local MODULE = GAS.Logging:MODULE()
 
 MODULE.Category = "[NNT] Anti-AFK"
-MODULE.Name     = "AKF LOGS"
+MODULE.Name     = "AFK Logs"
 MODULE.Colour   = Color(255, 149, 63)
 
 MODULE:Hook("AikoAfkTimeBefore","AikoAfkTimeBefore",function(ply)
-	MODULE:Log(bLogs:FormatPlayer(ply) .. " is now AFK !")
+	MODULE:Log(GAS.Logging:FormatPlayer(ply) .. " is now AFK !")
 end)
 
 MODULE:Hook("AikoAfkTimeAfter","AikoAfkTimeAfter",function(ply)
-	MODULE:Log(bLogs:FormatPlayer(ply) .. " is no longer AFK !")
+	MODULE:Log(GAS.Logging:FormatPlayer(ply) .. " is no longer AFK !")
 end)
 
 MODULE:Hook("AikoAfkKICKa","AikoAfkKICKa",function(ply)
-	MODULE:Log(bLogs:FormatPlayer(ply) .. " is no longer afk since he has been kick for AFK !")
+	MODULE:Log(GAS.Logging:FormatPlayer(ply) .. " is no longer afk since he has been kick for AFK !")
 end)
 
 GAS.Logging:AddModule(MODULE)
@@ -29,7 +29,7 @@ MODULE.Name     = "AKF Kicks"
 MODULE.Colour   = Color(255, 149, 63)
 
 MODULE:Hook("AikoAfkKICK","AikoAfkKICK",function(ply)
-	MODULE:Log(bLogs:FormatPlayer(ply) .. " was {#H|kicked|#} for being AFK !")
+	MODULE:Log(GAS.Logging:FormatPlayer(ply) .. " was {#H|kicked|#} for being AFK !")
 end)
 
 GAS.Logging:AddModule(MODULE)
@@ -43,10 +43,10 @@ MODULE.Name     = "AFK Commands"
 MODULE.Colour   = Color(255, 149, 63)
 
 MODULE:Hook("AikoAfkCommands","AikoAfkCommands",function(ply,command)
-	MODULE:Log(bLogs:FormatPlayer(ply) .. " has executed the command: " .. command )
+	MODULE:Log(GAS.Logging:FormatPlayer(ply) .. " has executed the command: " .. command )
 end)
 MODULE:Hook("AikoAfkCommandsFail","AikoAfkCommandsFail",function(ply,command)
-	MODULE:Log(bLogs:FormatPlayer(ply) .. " has tried to use the command: " .. command )
+	MODULE:Log(GAS.Logging:FormatPlayer(ply) .. " has tried to use the command: " .. command )
 end)
 
 GAS.Logging:AddModule(MODULE)
