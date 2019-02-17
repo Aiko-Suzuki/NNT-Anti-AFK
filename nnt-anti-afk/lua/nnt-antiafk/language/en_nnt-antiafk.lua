aa39e1e3870ef6b76ff677793a957e7506291895
Aiko Suzuki <maxime-van@outlook.fr>
2019-02-16 20:54:44 -0500

    remove not used file

--- nnt-anti-afk/lua/nnt-antiafk/language/en_nnt-antiafk.lua
+++
@@ -1,9 +0,0 @@
-
-AntiAfkTranslate.EN = {
-	["MAINTEXT"] = "AFK WARNING !",
-	["WARN"] = "You are gonna be kicked for afk in",
-	["CANCEL"] = "Press any key to cancel !",
-	["REMOVEWARN"] = "Press any key remove the warning !",
-	["MOVEKEY"] = "(Moving Key / Space / Use key / Reload Key / Zoom key)",
-	["NAME"] = "English"
-}
--- nnt-anti-afk/lua/nnt-antiafk/language/fr_nnt-antiafk.lua
+++
@@ -1,9 +0,0 @@
-
-AntiAfkTranslate.FR = {
-	["MAINTEXT"] = "AFK AVERTISSEMENT!",
-	["WARN"] = "Vous allez être kick pour afk dans",
-	["CANCEL"] = "Appuyez sur n'importe quelle touche pour annuler!",
-	["REMOVEWARN"] = "Appuyez sur n'importe quelle touche pour supprimer l'avertissement!",
-	["MOVEKEY"] = "(Touche de Déplacement / Espace / Utiliser/ Recharger / Zoom)",
-	["NAME"] = "Français"
-}
